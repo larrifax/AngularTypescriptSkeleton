36ebaf300dd985d25b17e7a230c2eed115183b10
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-05 14:06:57 +0100

    Adds app.ts; the application's main bootstrap file

---
+++ app/app.ts
@@ -0,0 +1,19 @@
+/// <reference path="_Typings.ts" />
+
+module App {
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules
+        // Custom modules
+        // 3rd party modules
+    ]);
+
+    app.config(()=> {
+
+    });
+
+    app.run(()=> {
+
+    });
+}
