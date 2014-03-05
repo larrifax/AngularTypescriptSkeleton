f346315ff7770d5511e1e0015744725fa8e68146
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-05 15:56:55 +0100

    Adds a service for setting the page title

---
+++ app/services/PageTitle.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="../_Typings.ts" />
+'use strict';
+
+import config = require('../config');
+
+var serviceId = 'pageTitle';
+angular.module(config.Configuration.Application_Namespace).factory(serviceId, [PageTitle]);
+
+export interface IPageTitle {
+    title: string;
+}
+
+export class PageTitle implements IPageTitle {
+    get title(): string {
+        return this.title;
+    }
+
+    set title(value: string) {
+        this.title = value;
+    }
+}
