b4ef3433ead543f16f63c0ad26f7643f2c8d53a3
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-05 15:52:43 +0100

    Adds a service for string operations

---
+++ app/services/String.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../_Typings.ts" />
+'use strict';
+
+import config = require('../config');
+
+var serviceId = 'string';
+angular.module(config.Configuration.Application_Namespace).factory(serviceId, [String]);
+
+export interface IString {
+    format: (format: string, ...args: string[]) => string;
+}
+
+export class String implements IString {
+    public format(format: string, ...args: string[]): string {
+        return format.replace(/{(\d+)}/g, (match, index)=> {
+            return args[index] !== undefined ? args[index] : match;
+        });
+    }
+}
