f1f6328c7b34aaf00b59b9560275f883a0a94087
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-11 11:30:36 +0100

    Renames the PageTitle file

--- App/_Typings.ts
+++ App/_Typings.ts
@@ -1,6 +1,6 @@
 /// <reference path="Components/Main/Main.ts" />
 /// <reference path="Components/Main/MainController.ts" />
 /// <reference path="Components/Common/Common.ts" />
-/// <reference path="Components/Common/PageTitle.ts" />
+/// <reference path="Components/Common/PageTitleService.ts" />
 /// <reference path="Components/Common/StringService.ts" />
 /// <reference path="../Scripts/typings/angularjs/angular.d.ts" />
