11cab13d40e8d270a4303d2cfb5bddaf2ffa0cdf
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 16:31:52 +0100

    Refactors the StringService tests to use the TestHelper class

--- App/Components/Common/StringService.spec.ts
+++ App/Components/Common/StringService.spec.ts
@@ -3,34 +3,38 @@
 module Components.Common.Tests {
     'use strict';
 
+    import TestHelper = App.Common.TestHelper;
+
     describe("StringService", () => {
-        var commonStringService: Services.IStringService;
 
         beforeEach(module(Components.Common.Common.ID));
 
-        beforeEach(inject((_commonStringService_)=> {
-            commonStringService = _commonStringService_;
-        }));
+        describe("formatting function", () => {
 
-        describe("formatting function", ()=> {
+            var stringService: Components.Common.Services.StringService;
+
+            beforeEach(inject(($injector) => {
+                stringService = TestHelper.serviceInjector<Components.Common.Services.StringService>($injector, Components.Common.Services.StringService.ID);
+            }));
+
             it("should format instances of {number}", () => {
-                expect(commonStringService.format("{0}", "Hello")).toBe("Hello");
+                expect(stringService.format("{0}", "Hello")).toBe("Hello");
             });
 
             it("should support multiple instances of {number}", () => {
-                expect(commonStringService.format("{0} {1}{2}", "Hello", "World", "!")).toBe("Hello World!");
+                expect(stringService.format("{0} {1}{2}", "Hello", "World", "!")).toBe("Hello World!");
             });
 
             it("should support multiple instances of the same {number}", () => {
-                expect(commonStringService.format("{0} {0}", "Hello")).toBe("Hello Hello");
+                expect(stringService.format("{0} {0}", "Hello")).toBe("Hello Hello");
             });
 
             it("should not replace instances of {number} if there is no argument for number", () => {
-                expect(commonStringService.format("{0} {1}", "Hello")).toBe("Hello {1}");
+                expect(stringService.format("{0} {1}", "Hello")).toBe("Hello {1}");
             });
 
             it("should not format instances of {}", () => {
-                expect(commonStringService.format("{}", "Hello")).toBe("{}");
+                expect(stringService.format("{}", "Hello")).toBe("{}");
             });
         });
     });
