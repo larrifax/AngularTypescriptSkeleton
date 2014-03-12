7ea147a89b3941125b5a22c79a8b559b3b7b947f
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 16:28:26 +0100

    Refactors the PageTitleService tests to use the TestHelper class

--- App/Components/Common/PageTitleService.spec.ts
+++ App/Components/Common/PageTitleService.spec.ts
@@ -3,6 +3,8 @@
 module Components.Common.Tests {
     'use strict';
 
+    import TestHelper = App.Common.TestHelper;
+
     describe("PageTitleService", () => {
 
         beforeEach(module(Components.Common.Common.ID));
@@ -19,30 +21,36 @@ module Components.Common.Tests {
                 App.Config.Configuration.Application_Title = original;
             });
 
-            it("should initially be set to null", () => {
+            it("should initially be set to null", inject(($injector) => {
                 App.Config.Configuration.Application_Title = "Test";
 
-                inject((commonPageTitleService)=> {
-                    expect(commonPageTitleService.title).toBe("Test");
-                });
-            });
+                var pageTitleService = TestHelper.serviceInjector<Components.Common.Services.PageTitleService>($injector, Components.Common.Services.PageTitleService.ID);
 
-            it("should be equal to application title when set to null", inject((commonPageTitleService) => {
-                App.Config.Configuration.Application_Title = "Test";
-                commonPageTitleService.title = null;
-                expect(commonPageTitleService.title).toBe("Test");
+                expect(pageTitleService.title).toBe("Test");
             }));
 
-            it("should be equal to application title when set to an empty string", inject((commonPageTitleService) => {
+            it("should be equal to application title when set to null", inject(($injector) => {
+                var pageTitleService = TestHelper.serviceInjector<Components.Common.Services.PageTitleService>($injector, Components.Common.Services.PageTitleService.ID);
+
                 App.Config.Configuration.Application_Title = "Test";
-                commonPageTitleService.title = "";
-                expect(commonPageTitleService.title).toBe("Test");
+                pageTitleService.title = null;
+                expect(pageTitleService.title).toBe("Test");
             }));
 
-            it("should use the form Title - Application Title when given a string", inject((commonPageTitleService) => {
+            it("should be equal to application title when set to an empty string", inject(($injector) => {
+                var pageTitleService = TestHelper.serviceInjector<Components.Common.Services.PageTitleService>($injector, Components.Common.Services.PageTitleService.ID);
+
                 App.Config.Configuration.Application_Title = "Test";
-                commonPageTitleService.title = "Title";
-                expect(commonPageTitleService.title).toBe("Title - Test");
+                pageTitleService.title = "";
+                expect(pageTitleService.title).toBe("Test");
+            }));
+
+            it("should use the form Title - Application Title when given a string", inject(($injector) => {
+                var pageTitleService = TestHelper.serviceInjector<Components.Common.Services.PageTitleService>($injector, Components.Common.Services.PageTitleService.ID);
+
+                App.Config.Configuration.Application_Title = "Test";
+                pageTitleService.title = "Title";
+                expect(pageTitleService.title).toBe("Title - Test");
             }));
         });
     });
