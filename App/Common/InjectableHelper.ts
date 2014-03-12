bc6736976d38804d4a5f90f532c608001ccb4a34
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 13:30:46 +0100

    Adds specialized methods for creating ids

--- App/Common/InjectableHelper.ts
+++ App/Common/InjectableHelper.ts
@@ -1,7 +1,26 @@
 module App.Common {
     'use strict';
 
+    var camelCase = (value: string) => value.charAt(0).toLowerCase() + value.slice(1);
+
     export class InjectableHelper {
+
+        static createControllerId(prefix: string, name: string) {
+            return camelCase(prefix + name + "Controller");
+        }
+
+        static createDirectiveId(prefix: string, name: string) {
+            return camelCase(prefix + name + "Directive");
+        }
+
+        static createServiceId(prefix: string, name: string) {
+            return camelCase(prefix + name + "Service");
+        }
+
+        static createFilterId(prefix: string, name: string) {
+            return camelCase(prefix + name + "Filter");
+        }
+
         static createId(prefix: string, name: string) {
             return prefix == null || prefix === "" ? name : prefix + "." + name;
         }
