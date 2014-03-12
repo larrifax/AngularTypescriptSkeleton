a33a55d7ab693385bafce145b39547b473cd0114
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 13:29:36 +0100

    Adds typings for angular-mocks and jasmine

--- App/_Typings.ts
+++ App/_Typings.ts
@@ -1,3 +1,5 @@
+/// <reference path="../Scripts/typings/angularjs/angular-mocks.d.ts" />
+/// <reference path="../Scripts/typings/jasmine/jasmine.d.ts" />
 /// <reference path="Components/Main/Main.ts" />
 /// <reference path="Components/Main/MainController.ts" />
 /// <reference path="Components/Common/Common.ts" />
