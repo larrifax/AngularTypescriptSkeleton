34987f672637c485b1fbcf25c53f8b0a6fbf763c
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 13:31:49 +0100

    Adds unit tests for the StringService

---
+++ App/Components/Common/StringService.spec.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../_Typings.ts" />
+
+module Components.Common.Tests {
+    'use strict';
+
+    describe("StringService", () => {
+        var commonStringService: Services.IStringService;
+
+        beforeEach(module(Components.Common.Common.ID));
+
+        beforeEach(inject((_commonStringService_)=> {
+            commonStringService = _commonStringService_;
+        }));
+
+        describe("formatting function", ()=> {
+            it("should format instances of {number}", () => {
+                expect(commonStringService.format("{0}", "Hello")).toBe("Hello");
+            });
+
+            it("should support multiple instances of {number}", () => {
+                expect(commonStringService.format("{0} {1}{2}", "Hello", "World", "!")).toBe("Hello World!");
+            });
+
+            it("should not replace instances of {number} if there is no argument for number", () => {
+                expect(commonStringService.format("{0} {1}", "Hello")).toBe("Hello {1}");
+            });
+
+            it("should not format instances of {}", () => {
+                expect(commonStringService.format("{}", "Hello")).toBe("{}");
+            });
+        });
+    });
+}
