1f59a22295c997dc7c8f6cca8d06db0a775a7e27
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 13:39:17 +0100

    Removes unused configuration entry

--- App/Config/Config.ts
+++ App/Config/Config.ts
@@ -2,6 +2,5 @@ module App.Config {
     export class Configuration {
         static Application_Namespace: string = "App";
         static Application_Title: string = "Application Name";
-        static Application_Prefix: string = "app";
     }   
 }
