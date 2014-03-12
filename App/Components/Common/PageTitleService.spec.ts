c5c79f80c4053d3ba420be0020504c35a0210e74
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 15:34:01 +0100

    Refactors the PageTitleService tests

--- App/Components/Common/PageTitleService.spec.ts
+++ App/Components/Common/PageTitleService.spec.ts
@@ -4,36 +4,39 @@ module Components.Common.Tests {
     'use strict';
 
     describe("PageTitleService", () => {
-        var commonPageTitleService: Services.IPageTitleService;
 
         beforeEach(module(Components.Common.Common.ID));
 
-        beforeEach(inject((_commonPageTitleService_) => {
-            commonPageTitleService = _commonPageTitleService_;
-        }));
-
         describe("title", () => {
             it("should initially be set to null", () => {
-                expect(commonPageTitleService.title).toBe(null);
+                var commonPageTitleService;
+
+                App.Config.Configuration.Application_Title = "Test";
+
+                inject((_commonPageTitleService_)=> {
+                    commonPageTitleService = _commonPageTitleService_;
+                });
+
+                expect(commonPageTitleService.title).toBe("Test");
             });
 
-            it("should be equal to application title when set to null", () => {
+            it("should be equal to application title when set to null", inject((commonPageTitleService) => {
                 App.Config.Configuration.Application_Title = "Test";
                 commonPageTitleService.title = null;
                 expect(commonPageTitleService.title).toBe("Test");
-            });
+            }));
 
-            it("should be equal to application title when set to an empty string", () => {
+            it("should be equal to application title when set to an empty string", inject((commonPageTitleService) => {
                 App.Config.Configuration.Application_Title = "Test";
                 commonPageTitleService.title = "";
                 expect(commonPageTitleService.title).toBe("Test");
-            });
+            }));
 
-            it("should use the form Title - Application Title when given a string", () => {
+            it("should use the form Title - Application Title when given a string", inject((commonPageTitleService) => {
                 App.Config.Configuration.Application_Title = "Test";
                 commonPageTitleService.title = "Title";
                 expect(commonPageTitleService.title).toBe("Title - Test");
-            });
+            }));
         });
     });
 }
