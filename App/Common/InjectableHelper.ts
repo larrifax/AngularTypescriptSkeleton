d1eedea0f32326654e803fa4bbd87f35d1e28694
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-12 13:58:20 +0100

    Changes ids for controllers to the form "Prefix.NameController"

--- App/Common/InjectableHelper.ts
+++ App/Common/InjectableHelper.ts
@@ -2,11 +2,12 @@ module App.Common {
     'use strict';
 
     var camelCase = (value: string) => value.charAt(0).toLowerCase() + value.slice(1);
+    var capitalize = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
 
     export class InjectableHelper {
 
         static createControllerId(prefix: string, name: string) {
-            return camelCase(prefix + name + "Controller");
+            return capitalize(prefix + "." + name + "Controller");
         }
 
         static createServiceId(prefix: string, name: string) {
