ab87a8bff084975a3266c384a87343661e507f89
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2014-03-14 14:21:36 +0100

    Adds gulp tasks for protractor

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,28 +2,42 @@ declare var require;
 
 var gulp = require('gulp');
 var karma = require('gulp-karma');
+var prot = require('gulp-protractor').protractor;
 
-var testFiles = [
-    'client/todo.js',
-    'client/todo.util.js',
-    'client/todo.App.js',
-    'test/client/*.js'
-];
+var karmaFiles = ['non_existing_file.js'];
+var e2eFiles = ['app/**/*_e2e.js'];
+
 
 gulp.task('test', () => {
-    return gulp.src(testFiles)
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'run'
-        })).on('error', err=> {
-            throw err;
-        });
+    return gulp.src(karmaFiles).pipe(karma({
+        configFile: 'karma.conf.js',
+        action: 'run'
+    })).on('error', err=> {
+       throw err;
+    });
+});
+
+gulp.task('e2e', () => {
+    return gulp.src(e2eFiles)
+        .pipe(prot({
+            configFile: "protractor_conf.js",
+            args: ['--baseUrl', 'http://localhost:61916']
+        }))
+        .on('error', e=> { throw e; });
+});
+
+gulp.task('e2edebug', () => {
+    return gulp.src(e2eFiles)
+        .pipe(prot({
+            configFile: "protractor_conf.js",
+            args: ['debug', '--baseUrl', 'http://localhost:61916']
+        }))
+        .on('error', e=> { throw e; });
 });
 
 gulp.task('default', ()=> {
-    gulp.src(testFiles)
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'watch'
-        }));
+    gulp.src(karmaFiles).pipe(karma({
+        configFile: 'karma.conf.js',
+        action: 'watch'
+    }));
 });
